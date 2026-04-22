9e67e466298a18446673c3afe5d9bf7bdc1c385f
CONt12006 <162928166+CONt12006@users.noreply.github.com>
2026-04-22 12:13:19 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://duckuser:duckpass@localhost/duckbot_db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
